f5f5b892088508e7c9165e988c6c763063600773
WeiQiPro <jaeldonley@gmail.com>
2022-10-30 01:38:24 -0400

    moved graphics and boardStates to gobanUI initializing

--- SPC/goban/UserInterface.js
+++ SPC/goban/UserInterface.js
@@ -2,6 +2,11 @@ let gobanUI = {
   initialize: () => {
     gobanUI.visual().fillStyle = 'white'
     gobanUI.visual().fillRect(0, 0, 1920, 1080)
+    gobanUI.boardIntersections = gobanUI.board.intersectionKeymap
+    gobanUI.boardLabels = gobanUI.board.labelKeymap
+
+    gobanUI.board.initialize(gobanUI.size)
+    gobanUI.boardGraphics.initializeBoardGraphics(gobanUI)
   },
   canvas: () => document.querySelector('#gobanCanvas'),
   visual: () => gobanUI.canvas().getContext('2d'),
@@ -14,9 +19,4 @@ let gobanUI = {
   boardLabels: '',
 }
 
-gobanUI.board.initialize(gobanUI.size)
-gobanUI.boardIntersections = gobanUI.board.intersectionKeymap
-gobanUI.boardLabels = gobanUI.board.labelKeymap
 gobanUI.initialize()
-
-gobanUI.boardGraphics.initializeBoardGraphics(gobanUI)
