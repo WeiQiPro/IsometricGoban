cf9f91e9c9efd76096a7d91e02322a363af3eb2b
WeiQiPro <jaeldonley@gmail.com>
2022-11-06 19:18:10 -0500

    mouse functions pt 1

--- goban/Augmentation.js
+++ goban/Augmentation.js
@@ -1,4 +1,8 @@
 let DataStructure = {
+  functions: (goban, dataStructure) => {
+    DataStructure.Cursor.coordinates.onmousemove(goban, dataStructure)
+
+  },
   players:{
     ID:{
       1: undefined,
@@ -27,25 +31,73 @@ let DataStructure = {
   Cursor: {
     hoveredIntersection: undefined,
     coordinates: {
-      onmousemove: () => {},
-      screen: {
-        x: () =>{},
-        y: () =>{}
+      onmousemove: (goban, dataStructure) => {
+       goban.Cursor.canvas().addEventListener("mousemove", (e) => {
+          let cursorX = e.clientX - Math.round(window.scrollX);
+          let cursorY = e.clientY - Math.round(window.scrollY);
+          dataStructure.Cursor.canvas = CoordinatesScreenToCanvas.function(cursorX, cursorY);
+       })
       },
       canvas: {
-        x: () => {},
-        y: () => {}
+        x: 0,
+        y: 0
       }
+
     }
   }
 }
 
 let CoordinatesScreenToCanvas = {
-  MatrixToInverseMatrix: () => {},
-  function: () => {}
+  MatrixToInverseMatrix: () => {
+    let matrix = Goban.UI.matrix
+    let inverseMatrix = Goban.UI.inverseMatrix
+    crossProduct = matrix[0] * matrix[3] - matrix[1] * matrix[2];
+
+    inverseMatrix[0] = matrix[3] / crossProduct;
+    inverseMatrix[1] = -matrix[1] / crossProduct;
+    inverseMatrix[2] = -matrix[2] / crossProduct;
+    inverseMatrix[3] = matrix[0] / crossProduct;
+
+  },
+  function: (x, y) => {
+    let matrix = Goban.UI.matrix
+    let inverseMatrix = Goban.UI.inverseMatrix
+    let xx, yy, result;
+    xx = x - matrix[4];
+    yy = y - matrix[5];
+    return {
+      x: xx * inverseMatrix[0] + yy * inverseMatrix[2],
+      y: xx * inverseMatrix[1] + yy * inverseMatrix[3],
+    };}
 }
 
-let FindNearestIntersection = function(){}
+let FindNearestIntersection = function(mouse, intersection){
+        let modulator = intersection.Height
+        let modulatorHalf = modulator/2
+        let mouseX = mouse.canvas.position.x
+        let mouseY = mouse.canvas.position.y
 
-CoordinatesScreenToCanvas.MatrixToInverseMatrix()
-CoordinatesScreenToCanvas.function()
+        let modmouseX = mouseX % modulator
+        let modmouseY = mouseY % modulator
+        let immediate = {
+            intersection: {
+                x: null,
+                y: null
+            }
+        }
+
+        if(modmouseX >= modulatorHalf){
+            immediate.intersection.x = (mouseX + (modulator - modmouseX))
+        } else if (modmouseX <= modulatorHalf) {
+            immediate.intersection.x = (mouseX - modmouseX)
+        }
+
+        if(modmouseY >= modulatorHalf){
+            immediate.intersection.y = (mouseY + (modulator - modmouseY))
+        } else if (modmouseY <= modulatorHalf) {
+            immediate.intersection.y = (mouseY - modmouseY)
+        }
+
+     DataStructure.Cursor.hoveredIntersection = jsBoard.intersectionKey[[immediate.intersection.x, immediate.intersection.y]]
+
+}
--- goban/UserInterface.js
+++ goban/UserInterface.js
@@ -6,12 +6,13 @@ let Goban = {
     letters: ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"],
     numbers: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
     },
-    matrix: [2, 1, -2, 1, 600, 000],
+    matrix:[2, 1, -2, 1, 600, 000],
     inverseMatrix: [],
     initialize: (UI) => {
       Goban.UI.board.initialize(UI),
       DataStructure.Goban.board.dataInitialize(UI)
       Goban.Graphics.controller.initializeBoard(UI)
+      CoordinatesScreenToCanvas.MatrixToInverseMatrix()
     },
   },
   Graphics: {
@@ -20,7 +21,6 @@ let Goban = {
     visual: () => Goban.Graphics.canvas().getContext('2d'),
   },
   Cursor: {
-    onmousedown: () => {},
     canvas: () => document.querySelector('#cursorCanvas'),
     visual: () => Goban.Cursor.canvas().getContext('2d'),
   },
@@ -30,4 +30,5 @@ let Goban = {
   }
 }
 
+DataStructure.functions(Goban, DataStructure)
 Goban.UI.initialize(Goban.UI)
